d09b9ddc8235488f6d29d4fd9d8a261d320ac991
linmo-33 <luo-111@qq.com>
2023-12-18 09:39:26 +0800

    fix bug

---
+++ src/main/java/com/linmo/oj/mapper/SysUserRoleMapper.java
@@ -0,0 +1,18 @@
+package com.linmo.oj.mapper;
+
+import com.linmo.oj.model.sysrole.SysUserRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author ljl
+* @description 针对表【sys_user_role】的数据库操作Mapper
+* @createDate 2023-12-15 10:21:46
+* @Entity com.linmo.oj.model.sysrole.SysUserRole
+*/
+public interface SysUserRoleMapper extends BaseMapper<SysUserRole> {
+
+}
+
+
+
+
---
+++ src/main/java/com/linmo/oj/service/SysUserRoleService.java
@@ -0,0 +1,13 @@
+package com.linmo.oj.service;
+
+import com.linmo.oj.model.sysrole.SysUserRole;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author ljl
+* @description 针对表【sys_user_role】的数据库操作Service
+* @createDate 2023-12-15 10:21:46
+*/
+public interface SysUserRoleService extends IService<SysUserRole> {
+
+}
